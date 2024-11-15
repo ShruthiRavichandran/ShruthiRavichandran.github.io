ee8cca360dd45381b1840c362fcbfea99214e868
Shruthi Ravichandran <shruthiravi@Shruthis-MacBook-Pro.local>
2024-11-15 00:08:39 -0500

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("mousemove", function(e) {
+    const cursor = document.querySelector(".cursor");
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+});
+
